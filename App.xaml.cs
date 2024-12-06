a9fcc2d6928ddeefd61c4991b48ae9df3dc93a60
sbloomztf <71042371+sbloomztf@users.noreply.github.com>
2024-12-06 11:31:50 -0600

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiWindowsNavsAJerk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ NotMainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiWindowsNavsAJerk
+{
+    public partial class NotMainPage : ContentPage
+    {
+        int count = 0;
+
+        public NotMainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+
+            /*
+
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);*/
+        }
+    }
+
+}
